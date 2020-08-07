a727d14ac31c92d202635c221686a94517b55df7
arifulshahed <arfshahedlinks@gmail.com>
2020-08-07 23:13:58 +0600

    trueVSfalse and nullVSundefined

---
+++ nullVSundefined.js
@@ -0,0 +1,22 @@
+//let name;
+
+// function add(num1, num2){
+//     console.log(num1 + num2)
+//     return 
+// }
+
+function add(num1, num2 = 19){
+    console.log(num1, num2)
+    
+}
+
+const result = add(4);
+console.log(result)
+
+
+const player = {name:"Karim Banzema", team:"Real Madrid C.F.", country:"France", contact:384738};
+console.log(player.country)
+console.log(player.birthDate)
+
+let name = undefined;
+console.log(name)
---
+++ trueVSfalse.js
@@ -0,0 +1,18 @@
+//falsy values
+
+//  0  ,   ""  ,  undefined   ,  null  ,  NaN,  false
+
+// truethy values
+
+//   "0"   ,   " "   ,   []   ,   {}
+
+
+//const name = "0";
+let name = 0;
+
+if(name || name == 0 ){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
