b4c63806d5181ba42431fb54d28e861253db985d
arifulshahed <arfshahedlinks@gmail.com>
2020-08-08 22:10:13 +0600

    module 24 done

---
+++ array-split.js
@@ -0,0 +1,8 @@
+const numbers = [23, 12, 23, 45, 43 ,54 ,5];
+//const part = numbers.slice(1, 4);
+
+const remove = numbers.splice(1, 4);
+console.log(remove)
+
+//console.log(part)
+console.log(numbers)
